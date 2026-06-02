728f64004bacba609802838d7d89042588e24355
Frank Boerman <frank@fboerman.nl>
2026-06-02 18:49:43 +0200

    improve styling

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"main/statuspage"
+	"os"
 	"time"
 
 	"charm.land/huh/v2"
@@ -11,28 +12,24 @@ import (
 	"charm.land/lipgloss/v2/table"
 )
 
-const (
-	purple    = "99"
-	gray      = "245"
-	lightGray = "241"
-)
-
 var (
-	// HeaderStyle is the lipgloss style used for the table headers.
-	HeaderStyle = lipgloss.NewStyle().
-			Foreground(lipgloss.Color(purple)).
-			Bold(true).
-			Align(lipgloss.Left)
-	// CellStyle is the base lipgloss style used for the table rows.
 	CellStyle = lipgloss.NewStyle().
 			Padding(0, 1).
 			Width(14)
-	// OddRowStyle is the lipgloss style used for odd-numbered table rows.
-	OddRowStyle = CellStyle.Foreground(lipgloss.Color(gray))
-	// EvenRowStyle is the lipgloss style used for even-numbered table rows.
-	EvenRowStyle = CellStyle.Foreground(lipgloss.Color(lightGray))
-	// BorderStyle is the lipgloss style used for the table border.
+	RowStyle = CellStyle.Foreground(lipgloss.White).
+			BorderStyle(lipgloss.NormalBorder()).
+			BorderBottom(true)
 	BorderStyle = lipgloss.NewStyle().Foreground(lipgloss.White)
+	HeaderStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#f59e0b")).
+			Bold(true).
+			Align(lipgloss.Left)
+)
+
+var (
+	Green  = "#65a30d"
+	Yellow = "#fbbf24"
+	Red    = "#b91c1c"
 )
 
 func askDate() (string, error) {
@@ -60,18 +57,23 @@ func askDate() (string, error) {
 
 func printTable(table_data [][]string) {
 	t := table.New().
-		Border(lipgloss.ThickBorder()).
+		Border(lipgloss.RoundedBorder()).
 		BorderStyle(BorderStyle).
 		StyleFunc(func(row, col int) lipgloss.Style {
 			var style lipgloss.Style
 
-			switch {
-			case row == table.HeaderRow:
-				return HeaderStyle
-			case row%2 == 0:
-				style = EvenRowStyle
-			default:
-				style = OddRowStyle
+			if row == table.HeaderRow {
+				style = HeaderStyle
+			} else {
+				style = RowStyle
+			}
+
+			if col == 0 {
+				style = style.Bold(true).Foreground(lipgloss.Color("#FACD81"))
+			}
+
+			if row == len(table_data)-2 {
+				style = style.BorderBottom(false)
 			}
 
 			style = style.Align(lipgloss.Left)
@@ -85,24 +87,33 @@ func printTable(table_data [][]string) {
 }
 
 func main() {
+	var business_day string
+	var err error
 
-	business_day, err := askDate()
-	if err != nil {
-		log.Fatal(err)
+	if len(os.Args) > 1 {
+		business_day = os.Args[1]
+	} else {
+		business_day, err = askDate()
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-
 	tables, err := statuspage.GetData(business_day)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	lipgloss.Println("Flowbased Capacity Calculation")
+	lipgloss.Println(HeaderStyle.Render("Core Market Coupling Status\n" +
+		fmt.Sprintf("For business day %s \n", business_day) +
+		"An Amun Analytics product\n"))
+
+	lipgloss.Println(HeaderStyle.Render("> Flowbased Capacity Calculation"))
 	printTable(tables.FBCC)
 
-	lipgloss.Println("Day-Ahead Market Coupling")
+	lipgloss.Println(HeaderStyle.Render("> Day-Ahead Market Coupling"))
 	printTable(tables.DAMC)
 
-	lipgloss.Println("Intraday Market Coupling")
+	lipgloss.Println(HeaderStyle.Render("> Intraday Market Coupling"))
 	printTable(tables.IDMC)
 
 }
